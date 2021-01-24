bf754c7f118a0527566d17f61251d20ef7da1c94
Keskin <62107730+OmerSahinKeskin@users.noreply.github.com>
2021-01-24 16:45:17 +0100

    Robin&Ömer Changed code structure

---
+++ mavenproject1/src/main/webapp/JS/ButtonClick.js
@@ -0,0 +1,4 @@
+var link = document.querySelector('link[rel="import"][href="/assets/components/SideBar.html"]');
+console.log(link);
+var templateContent = link.import.querySelector('body');
+console.log(templateContent);
