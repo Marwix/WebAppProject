7cfd6b4019c30ae33311bb77b45443354828d56d
Keskin <62107730+OmerSahinKeskin@users.noreply.github.com>
2021-01-24 23:36:23 +0100

    Ömer&Robin added tank on the list of selling items

--- mavenproject1/src/main/webapp/JS/ButtonClick.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function() { 
-    const buttons = document.querySelectorAll('.list-group-item');
-    for (const btn of buttons){
-        btn.addEventListener('click', function() {
-            console.log(btn.id);
-        })
-    }
- }
