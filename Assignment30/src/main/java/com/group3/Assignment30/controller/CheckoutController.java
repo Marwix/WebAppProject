5cb9a2efd68d6807b8988c3086dde7f66d6dc28f
Marwix <allan-ridha@live.se>
2021-03-04 16:23:54 +0100

    Fixed Coupon and Discount apply

--- Assignment30/src/main/java/com/group3/Assignment30/controller/CheckoutController.java
+++ Assignment30/src/main/java/com/group3/Assignment30/controller/CheckoutController.java
@@ -1,8 +1,10 @@
 package com.group3.Assignment30.controller;
 
+import com.group3.Assignment30.model.dao.CouponDAO;
 import com.group3.Assignment30.model.dao.CustomerDAO;
 import com.group3.Assignment30.model.dao.ProductDAO;
 import com.group3.Assignment30.model.dao.PurchaseDAO;
+import com.group3.Assignment30.model.entity.Coupon;
 import com.group3.Assignment30.model.entity.Customer;
 import com.group3.Assignment30.model.entity.Product;
 import com.group3.Assignment30.model.entity.Purchase;
@@ -19,6 +21,7 @@ import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -42,6 +45,9 @@ public class CheckoutController implements Serializable {
     
     @EJB
     private ProductDAO productDAO;
+    
+    @EJB
+    private CouponDAO couponDAO;
     
     @EJB
     private CustomerDAO customerDAO;
@@ -100,6 +106,8 @@ public class CheckoutController implements Serializable {
         
         cartBackingBean.setCart(new HashMap<Product, Integer>());
         checkoutBackingBean.setProducts(new HashMap<Product, Integer>()); 
+        priceMultiplier = 1;
+        checkoutBackingBean.setCoupon("");
             
         ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         ec.redirect(ec.getRequestContextPath() + "/" + "paymentResult.xhtml");
@@ -115,7 +123,6 @@ public class CheckoutController implements Serializable {
         }
     }
   
-    
     // Count the number of products available. 
     public int getCount(Product product)
     {
@@ -138,15 +145,62 @@ public class CheckoutController implements Serializable {
     // Check if coupon code is valid.
     public void checkValidCoupon()
     {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        Map<String,String> params = facesContext.getExternalContext().getRequestParameterMap();
-        String couponCode = params.get("action");
+        String code = checkoutBackingBean.getCoupon();
         
-        // Test data.
-        priceMultiplier = 0.8;
+        List<Coupon> couponCodes = couponDAO.getCouponByCode(code);
+        if (couponCodes.size() > 0)
+        {
+           if (priceMultiplier != 1) {
+               if (priceMultiplier < couponCodes.get(0).getPriceMultiplier()) {
+                   priceMultiplier = couponCodes.get(0).getPriceMultiplier();
+                   System.out.println("Better code applied.");
+                   sendNotification(FacesMessage.SEVERITY_INFO, "Better code applied!");
+               }
+           }
+           else {
+               priceMultiplier = couponCodes.get(0).getPriceMultiplier();
+               sendNotification(FacesMessage.SEVERITY_INFO, "Coupon code applied!");
+               System.out.println("Coupon code applied.");
+           }  
+        }
+        else
+        {
+            sendNotification(FacesMessage.SEVERITY_INFO, "Not a valid code!");
+            System.out.println("Not a valid code. ");
+        }
     }
     
+    public void sendNotification(FacesMessage.Severity severity, String message){
+            FacesMessage fMessage = new FacesMessage();
+            fMessage.setSeverity(severity);
+            fMessage.setSummary(message);
+            FacesContext.getCurrentInstance().addMessage(null,fMessage);
+    }
+    
+    // Check if coupon is applied or not.
     public boolean CouponApplied() {
         return priceMultiplier != 1; 
+    }
+    
+    // Remove item from checkoutpage cart.
+    public void removeItemCart() throws IOException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Map<String,String> params = facesContext.getExternalContext().getRequestParameterMap();
+        String id = params.get("action");
+       
+        HashMap<Product,Integer> listOfItemsCart = cartBackingBean.getCart();
+        
+        for (Product product : listOfItemsCart.keySet())
+        {
+            if (product.getProdoct_id() == Integer.parseInt(id))
+            {
+                listOfItemsCart.remove(product);
+                cartBackingBean.setCart(listOfItemsCart);
+            }
+        }
+        
+        // Refresh
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.redirect(((HttpServletRequest)ec.getRequest()).getRequestURI());
     }
 }
