f428ee4da2fa9a3a8195d9c638a16505661a194d
Keskin <62107730+OmerSahinKeskin@users.noreply.github.com>
2021-03-05 19:24:44 +0100

    Robin&Ömer -- changed cart, checkoutpage, changed purchase history on request of Robin Adams(not sure if we did it correct or not check on monday)

--- Assignment30/src/main/java/com/group3/Assignment30/views/CheckoutBackingBean.java
+++ Assignment30/src/main/java/com/group3/Assignment30/views/CheckoutBackingBean.java
@@ -4,35 +4,121 @@
  * and open the template in the editor.
  */
 package com.group3.Assignment30.views;
+import com.group3.Assignment30.model.entity.Cart;
+import com.group3.Assignment30.model.entity.Coupon;
+import com.group3.Assignment30.model.entity.Customer;
 import com.group3.Assignment30.model.entity.Product;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Init;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 @Named
 @SessionScoped
 public class CheckoutBackingBean implements Serializable {
-    @NotEmpty private String country;
-    @NotEmpty private String coupon;
-    @Email @NotEmpty private String email;
-    @NotEmpty private String firstname;
-    @NotEmpty private String lastname;
-    @NotEmpty private String address;
-    @NotEmpty private String city;
-    @NotEmpty private String postcode;
-    @NotEmpty private String phonenumber;
-    private HashMap<Product,Integer> products;
     
-    public void setCode(String code) {
-        this.coupon = code;
+    private Customer customer;
+    private Coupon coupon;
+    @EJB
+    private Cart cart;
+    
+    @PostConstruct
+    public void Init(){
+        coupon = new Coupon();
+        coupon.setCouponCode("");
+        coupon.setPriceMultiplier(1);
     }
     
-    public String getText() {
-        return coupon;
+    public List<Product> getKeyList()
+    {  try {
+            
+        return new ArrayList<>(cart.getCartInventory().keySet());
+        } catch (NullPointerException e) {
+            return new ArrayList<Product>();
+        }
     }
+    
+    public int getCount(Product product)
+    {
+        return cart.getCartInventory().get(product);
+    }
+    
+    public void removeItemCart(Product product){
+
+        if(cart.getCartInventory().containsKey(product)){
+            cart.getCartInventory().remove(product);
+        }
+
+        if (cart.getCartInventory().isEmpty()) {
+            coupon = new Coupon();
+            coupon.setCouponCode("");
+            coupon.setPriceMultiplier(1);
+        }
+
+    }
+    
+    public String applyCoupon(List<Coupon> couponCode)
+    {
+        Coupon newCoupon = couponCode.get(0);
+        
+        
+           if (coupon.getPriceMultiplier() != 1) {
+               
+               if (coupon.getPriceMultiplier() < newCoupon.getPriceMultiplier()) {
+                   coupon = newCoupon;
+                   return "Better code applied!";
+               }
+           }else {
+               coupon = newCoupon;
+               return "Coupon code applied!";
+           }  
+           return "";
+    }
+    
+    // Check if coupon is applied or not.
+    public boolean CouponApplied() {
+        return coupon.getPriceMultiplier() != 1; 
+    }
+    
+      //Add price of all items in cart and return total.
+    public double getTotalPrice()
+    {
+        double totalPrice = 0;
+        
+        List<Product> items = getKeyList();
+        
+        for (Product s : items)   
+        {
+            totalPrice += s.getPrice() * getCount(s);
+        }
+        return totalPrice;
+    }
+    public boolean resetCartAndCoupon(){
+        
+       cart.setCartInventory(new HashMap<Product, Integer>());
+       coupon.setPriceMultiplier(1);
+       return true;
+    }
+    
+//    @NotEmpty private String country;
+//    @NotEmpty private String coupon;
+//    
+//    private HashMap<Product,Integer> products;
+//    
+//    public void setCode(String code) {
+//        this.coupon = code;
+//    }
+//    
+//    public String getText() {
+//        return coupon;
+//    }
+
 }
