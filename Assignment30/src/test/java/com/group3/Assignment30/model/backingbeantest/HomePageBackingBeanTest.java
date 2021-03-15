274cb945df088c1be455115ebe15c8053953bac7
Mojtaba ataie <mojtaba.ataie99@gmail.com>
2021-03-15 15:28:25 +0100

    HomePageBackingBeanTest

--- Assignment30/src/test/java/com/group3/Assignment30/model/backingbeantest/HomePageBackingBeanTest.java
+++ Assignment30/src/test/java/com/group3/Assignment30/model/backingbeantest/HomePageBackingBeanTest.java
@@ -1,12 +1,9 @@
-
 package com.group3.Assignment30.model.backingbeantest;
-
 
 import com.group3.Assignment30.model.entity.Product;
 import com.group3.Assignment30.views.HomePageBackingBean;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -19,8 +16,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class HomePageBackingBeanTest {
-    
-    
     HomePageBackingBean homePageBackingBean;
     private Product product1;
    
@@ -31,32 +26,15 @@ public class HomePageBackingBeanTest {
                 .addAsResource("META-INF/persistence.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
-    
-   
-    
-    
+
     @Before
     public void init() {
         homePageBackingBean = new HomePageBackingBean();
 
         product1 = new Product(1, "Catapult", 100, 1, 3, "blue", "test", "20kg", "test description 1");
-        
-//        product1 = new Product();
-//        product1.setProdoct_id(1);
-//        product1.setProduct_name("Catapult");
-//        product1.setPriceMultiplier(1);
-//        product1.setFullStar(3);
-//        product1.setColor("blue");
-//        product1.setMeasurements("test");
-//        product1.setWeight("20kg");
-//        product1.setDescription("test description");
-//        
-//        productDAO.create(product1);
-        
+ 
     }
-    
-    
-    
+
     @Test
     public void getterAndSetterTest() {
        
@@ -67,8 +45,5 @@ public class HomePageBackingBeanTest {
         homePageBackingBean.setProducts(products);
         
         assertEquals(products,homePageBackingBean.getProducts());
-       
-        
     }
-    
 }
